9c16cd195dddc76c691fd82f187d4ee00d221f6a
jjalanis <jjalanis@gmail.com>
2018-05-13 17:32:19 -0500

    html 'done'

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+    $(document).ready(function() {
+
+
+/*
+GLOBAL VARIABLES
+targetScore
+totalWins
+totalLosses
+
+
+
+
+*/
+
+var targetScore;
+var totalWins;
+var otalLosses;
+
+
+
+
+
+
+
+
+
+
+
+    });
